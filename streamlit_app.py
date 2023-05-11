dd6e3cdfc96f02b2db9ebe411dea3293f2f32239
was-im <124791360+was-im@users.noreply.github.com>
2023-05-11 18:51:32 +0200

    Update streamlit_app.py
    Voeg alle code van mijn DeepNote toe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,93 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
+
 import streamlit as st
+import pandas as pd
+import subprocess
 
-"""
-# Welcome to Streamlit!
+# Install required packages
+subprocess.run(['pip', 'install', 'scikit-learn'])
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Load the dataset from GitHub
+data_url = 'https://github.com/was-im/streamlit-example/blob/master/adc.csv'
+data = pd.read_csv(data_url, encoding='utf-8', error_bad_lines=False, na_values=["NA", "N/A"])
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Select relevant columns
+selected_columns = ['age', 'sex', 'hours.per.week', 'native.country', 'workclass', 'capital.gain', 'capital.loss', 'income']
+data = data[selected_columns]
 
+# Convert categorical variables to numerical using label encoding
+label_encoder = LabelEncoder()
+data['sex'] = label_encoder.fit_transform(data['sex'])
+data['native.country'] = label_encoder.fit_transform(data['native.country'])
+data['workclass'] = label_encoder.fit_transform(data['workclass'])
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Split the data into features (X) and target (y)
+X = data.drop('income', axis=1)
+y = data['income']
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Train a Random Forest classifier
+clf = RandomForestClassifier(random_state=42)
+clf.fit(X, y)
 
-    points_per_turn = total_points / num_turns
+def main():
+    st.set_page_config(page_title="Adult Income Census Prediction", layout="wide")
+   
+    # Main content
+    st.title("Adult Income Census Prediction")
+    st.write("Customize the input parameters and click 'Predict' to see the income prediction result.")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    # Create input columns
+    col1, col2 = st.beta_columns(2)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    with col1:
+        st.subheader("Personal Information")
+        age = st.slider('Age', 18, 100, 10)
+        sex = st.radio('Sex', ('Male', 'Female'))
+
+    with col2:
+        st.subheader("Financial Information")
+        capital_gain = st.number_input('Capital Gain', 0)
+        capital_loss = st.number_input('Capital Loss', 0)
+
+    hours_per_week = st.slider('Hours Per Week', 0, 80, 8)
+
+    st.subheader("Additional Information")
+    country = st.selectbox('Country', data['native.country'].unique())
+    employment_type = st.selectbox('Employment Type', data['workclass'].unique())
+
+    # Make Prediction
+    if st.button('Predict'):
+        prediction = simulate_prediction(age, sex, capital_gain, capital_loss, hours_per_week, country, employment_type)
+
+        if prediction == 0:
+            st.warning('The income is below or equal to 50K')
+        else:
+            st.success('The income is above 50K')
+
+def simulate_prediction(age, sex, capital_gain, capital_loss, hours_per_week, country, employment_type):
+    # Prepare user input for prediction
+    user_input = pd.DataFrame({
+        'age': [age],
+        'sex': [sex],
+        'hours.per.week': [hours_per_week],
+        'native.country': [country],
+        'workclass': [employment_type],
+        'capital.gain': [capital_gain],
+        'capital.loss': [capital_loss]
+    })
+
+    # Convert categorical variables to numerical
+    user_input['sex'] = label_encoder.transform(user_input['sex'])
+    user_input['native.country'] = label_encoder.transform(user_input['native.country'])
+    user_input['workclass'] = label_encoder.transform(user_input['workclass'])
+
+    # Make a prediction using the trained model
+    prediction = clf.predict(user_input)[0]
+
+    return prediction
+
+if __name__ == '__main__':
+    main()
